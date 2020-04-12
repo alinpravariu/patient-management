e0877e0522ba6f55506071551994e8b28dbfe4cd
Alin Prăvariu <63364676+alinpravariu@users.noreply.github.com>
2020-04-13 00:43:30 +0300

    Updated ExaminationController.

--- src/main/java/org/alinpravariu/patientmanagement/controller/ExaminationController.java
+++ src/main/java/org/alinpravariu/patientmanagement/controller/ExaminationController.java
@@ -3,17 +3,26 @@ package org.alinpravariu.patientmanagement.controller;
 import org.alinpravariu.patientmanagement.entity.Examination;
 import org.alinpravariu.patientmanagement.entity.Patient;
 import org.alinpravariu.patientmanagement.entity.User;
+import org.alinpravariu.patientmanagement.observer.Observer;
 import org.alinpravariu.patientmanagement.repository.ExaminationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(path = "/patient-management")
 public class ExaminationController {
 
+    private List<Observer> observers;
+
     @Autowired
     private ExaminationRepository examinationRepository;
+
+    public ExaminationController(List<Observer> observers) {
+        this.observers = observers;
+    }
 
     @PostMapping(path = "/add-examination")
     public @ResponseBody String addNewExamination(@RequestParam User user, @RequestParam Patient patient, @RequestParam Examination.Status status) {
