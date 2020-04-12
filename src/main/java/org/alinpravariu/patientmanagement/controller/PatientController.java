6f9932d5e9934fc055b684f743cef57f01d2923b
Alin Prăvariu <63364676+alinpravariu@users.noreply.github.com>
2020-04-12 23:47:30 +0300

    Added PatientController controller.

---
+++ src/main/java/org/alinpravariu/patientmanagement/controller/PatientController.java
@@ -0,0 +1,36 @@
+package org.alinpravariu.patientmanagement.controller;
+
+import org.alinpravariu.patientmanagement.entity.Patient;
+import org.alinpravariu.patientmanagement.entity.User;
+import org.alinpravariu.patientmanagement.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/patient-management")
+public class PatientController {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @PostMapping(path = "/add-patient")
+    public @ResponseBody String addNewPatient(@RequestParam String ssn, @RequestParam String firstName, @RequestParam String lastName) {
+
+        Patient patient = new Patient();
+
+        patient.setSsn(ssn);
+        patient.setFirstName(firstName);
+        patient.setLastName(lastName);
+
+        patientRepository.save(patient);
+
+        return "Patient saved successfully.";
+    }
+
+    @GetMapping(path = "/all-patients")
+    public @ResponseBody Iterable<Patient> getAllPatients() {
+
+        return patientRepository.findAll();
+    }
+}
