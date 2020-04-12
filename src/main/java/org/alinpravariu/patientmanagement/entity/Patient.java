e101e6e9b38594214dc9a080a8652ebecfe82d65
Alin Prăvariu <63364676+alinpravariu@users.noreply.github.com>
2020-04-12 23:57:10 +0300

    Updated Patient.

--- src/main/java/org/alinpravariu/patientmanagement/entity/Patient.java
+++ src/main/java/org/alinpravariu/patientmanagement/entity/Patient.java
@@ -1,11 +1,9 @@
 package org.alinpravariu.patientmanagement.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "patients")
 public class Patient {
 
     @Id
