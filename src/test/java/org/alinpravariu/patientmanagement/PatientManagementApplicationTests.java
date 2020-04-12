b85d847388c1f4fb46a467edfad250999512ae28
Alin Prăvariu <63364676+alinpravariu@users.noreply.github.com>
2020-04-12 16:30:00 +0300

    Added Spring Boot project template.

---
+++ src/test/java/org/alinpravariu/patientmanagement/PatientManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alinpravariu.patientmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
