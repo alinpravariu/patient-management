cd9405a568b19691b9a1b36f54235ee81c594944
Alin Prăvariu <63364676+alinpravariu@users.noreply.github.com>
2020-04-13 00:21:37 +0300

    Update ExaminationRepository.

--- src/main/java/org/alinpravariu/patientmanagement/repository/ExaminationRepository.java
+++ src/main/java/org/alinpravariu/patientmanagement/repository/ExaminationRepository.java
@@ -1,7 +1,7 @@
 package org.alinpravariu.patientmanagement.repository;
 
-import org.alinpravariu.patientmanagement.entity.Patient;
+import org.alinpravariu.patientmanagement.entity.Examination;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ExaminationRepository extends CrudRepository<Patient, Integer> {
+public interface ExaminationRepository extends CrudRepository<Examination, Integer> {
 }
