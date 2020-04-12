9b4bd5fbe192ae79c3f806e32adf5f39c101341f
Alin Prăvariu <63364676+alinpravariu@users.noreply.github.com>
2020-04-13 00:09:45 +0300

    Added Examination entity.

---
+++ src/main/java/org/alinpravariu/patientmanagement/entity/Examination.java
@@ -0,0 +1,55 @@
+package org.alinpravariu.patientmanagement.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "examinations")
+public class Examination {
+
+    private enum Status{IN_PROGRESS, COMPLETED};
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "patient_id", referencedColumnName = "id")
+    private Patient patient;
+
+    private Status status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
